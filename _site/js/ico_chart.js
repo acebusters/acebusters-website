6695f865ff78eca5cbfd319d60c415ffb139a92e
Helge Wieding <helge@wieding.com>
2017-05-27 20:59:25 +0200

    new economy calculator

--- _site/js/ico_chart.js
+++ _site/js/ico_chart.js
@@ -1,79 +1,165 @@
-var canvas  = document.getElementById("icoChart");
-var ctx = document.getElementById("icoChart").getContext('2d');
-var myNutz = parseInt($('#my-nutz').val());
-var totalNutz = parseInt($('#total-nutz').val());
-var totalPower = parseInt($('#max-power').val());
-var myPower = parseInt($('#my-power-nutz').val());
-var firstRound = 12000000;
+/**
+ * Show Chart of our economy
+ */
 
+// charts
+document.addEventListener("DOMContentLoaded", function(e) {
+    Vue.component('power-pie', {
+        extends: VueChartJs.Pie,
+        mixins: [VueChartJs.mixins.reactiveProp],
+        mounted () {
+            this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false})
+        }    
+    });
 
-var calcData = function(totalNutz, myNutz, totalPower, myPower) {
-    totalNutz = parseInt(totalNutz);
-    myNutz = parseInt(myNutz);
-    totalPower = parseInt(totalPower);
-    myPower = parseInt(myPower);
-    var rest = totalNutz - myNutz - myPower;
-    var data = [];
-    data[0] = (myNutz / totalNutz) * 100;
-    data[1] = (myPower / totalNutz) * 100;
-    data[2] = (rest / totalNutz) * 100;
-    return data;
-};
+    Vue.component('nutz-pie', {
+        extends: VueChartJs.Pie,
+        mixins: [VueChartJs.mixins.reactiveProp],
+        mounted () {
+            this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false})
+        }    
+    });
 
-var initData = calcData(totalNutz, myNutz, totalPower, myPower);
-
-
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["My Nutz in supply %", "My Nutz in Power %", "Rest"],
-        datasets: [{
-            data: initData,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {}
+    var icoApp = new Vue({
+        delimiters:['<%', '%>'],
+        el: '#app',
+        data: {
+            activeSupply: 0,
+            powerPool: 1600000,
+            burnPool: 2400000,
+            ceiling: 100,
+            floor: 130,
+            alice: {
+                eth: 6000,
+                purNtz: 1200,
+                ntz: 0,
+                powerUp: 0,
+                abp: 0,
+                sellNtz: 0,
+                powerDown: 0,
+            },
+            bob: {
+                eth: 6000,
+                purNtz: 1200,
+                ntz: 0,
+                powerUp: 0,
+                abp: 0,
+                sellNtz: 0,
+                powerDown: 0,
+            },
+            authorizedPower: 100,
+            issuedPower: 60,
+            outstandingPower: 40,
+            totalReserve: 1,
+            needReserve: 0,
+            chart:{},
+        },
+        computed: {
+            totalNtz: function() {
+                return this.activeSupply + this.powerPool + this.burnPool;
+            },
+            dataNtzPie: function() {
+                return { 
+                    labels: ['Alice Ntz', 'Bobs Ntz', 'PowerPool', 'BurnPool'],
+                    datasets: [{
+                        label: 'Data One',
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        data: [this.alice.ntz, this.bob.ntz, this.powerPool, this.burnPool]
+                    }]
+                }
+            },
+            dataPowerPie: function() {
+                return { 
+                    labels: ['Alice ABP', 'Bobs ABP', 'Rest'],
+                    datasets: [{
+                        label: 'Data One',
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        data: [this.alice.abp, this.bob.abp, this.authorizedPower - this.alice.abp - this.bob.abp]
+                    }]
+                }
+            },
+        },
+        methods: {
+            purchaseNtz: function(amount, user) {
+                amount = parseInt(amount);
+                if (user.eth <= 0) {
+                    alert('Not enough Ether!');
+                    return;
+                }
+                this.activeSupply = this.activeSupply + (amount * this.ceiling);
+                user.eth -= amount;
+                user.ntz += (amount * this.ceiling);
+                this.issuePowerPool(user.ntz);
+            },
+            issuePowerPool: function(amount) {
+                this.powerPool += amount / this.issuedPower * this.outstandingPower;
+            },
+            sellNtz: function(amount, user) {
+                amount = parseInt(amount);
+                if (user.ntz <= 0) {
+                    alert('No more Nutz to sell!');
+                    return;
+                }
+                if (amount > user.ntz) {
+                    alert('Not enough Nutz!');
+                    return;
+                } 
+                this.activeSupply = this.activeSupply - amount;
+                user.ntz -= amount;
+                user.eth += (amount / this.floor);
+            },
+            powerUp: function(amount, user) {
+                amount = parseInt(amount);
+                if (user.ntz <= 0) {
+                    alert('No more Nutz to power up!');
+                    return;
+                }
+                if (amount > user.ntz) {
+                    alert('Not enough Nutz to power up!');
+                    return;
+                }
+                user.ntz -= amount;
+                this.activeSupply -= amount;
+                this.powerPool += amount;
+                user.abp = user.abp + (amount / this.totalNtz) * 100;
+                amount = parseInt(amount);
+            },
+            powerDown: function(amount, user) {
+                user.ntz += this.totalNtz * (amount / 100);
+                user.abp -= user.abp
+            },
+        }
+    });    
 });
 
-$('#my-nutz').change(function(e) {
-    if ((parseInt(this.value) + parseInt(myPower)) > firstRound) {
-        this.value = firstRound - parseInt(myPower);
-    }
-    myNutz = parseInt(this.value);
-    myChart.data.datasets[0].data = calcData(totalNutz, myNutz, totalPower, myPower);
-    myChart.update();
-});
-
-$('#my-power-nutz').change(function(e) {
-    if ((parseInt(this.value) + parseInt(myNutz)) > firstRound) {
-        this.value = firstRound - parseInt(myNutz);
-    }
-    myPower = parseInt(this.value);
-    myChart.data.datasets[0].data = calcData(totalNutz, myNutz, totalPower, myPower);
-    myChart.update();
-});
-
-$('#total-nutz').change(function(e) {
-    var powerRatio = myChart.data.datasets[0].data[1];
-    myPower = this.value * (powerRatio/100);
-    $('#my-power-nutz').val(myPower);
-    myChart.data.datasets[0].data = calcData(parseInt(this.value), myNutz, totalPower, myPower);
-    myChart.update();
-});
     
