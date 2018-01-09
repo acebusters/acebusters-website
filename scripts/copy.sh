f96ece29be83c8dabc968d119268e5063a7f7b02
Kosta Korenkov <kosta@whoot.me>
2018-01-09 14:14:28 +0300

    chore: cleanup build script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -4,18 +4,13 @@ set -e
 set -v
 set -o pipefail
 
-TEMPFILE='mktemp'
-rm -f $TEMPFILE
-
 echo "Copying files to server..."
-aws s3 sync $SOURCE $DEST_BUCKET --size-only --exclude "*" --include "*.*" --delete --acl public-read | tee -a $TEMPFILE
+aws s3 sync $SOURCE $DEST_BUCKET --size-only --exclude "*" --include "*.*" --delete --acl public-read
 
 echo "Copying files with content type..."
-aws s3 sync $SOURCE $DEST_BUCKET --size-only --content-type text/html --exclude "*.*" --delete --acl public-read | tee -a $TEMPFILE
+aws s3 sync $SOURCE $DEST_BUCKET --size-only --content-type text/html --exclude "*.*" --delete --acl public-read
 
 if [ "$DISTRIBUTION_ID" ]; then
   echo "Invalidating CloudFront distribution..."
   aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
 fi
-
-rm -f $TEMPFILE
